94037e42f6282035a22c5612f3e313a5cb088de5
iamlegend <deaviz15felix@gmail.com>
2024-01-08 12:59:04 +0100

    completed the project

--- web.js
+++ web.js
@@ -1,33 +1,50 @@
 "use strict";
 
 let secretnumber = Math.trunc(Math.random() * 20 + 1);
-
+let highscore = 0;
 let score = 20;
+let hig = document.querySelector(".hig");
+
 let start = document.querySelector(".start");
+let hit = document.querySelector(".hit");
+let guess = document.querySelector(".guess");
 let number = document.querySelector(".number");
 let question = document.querySelector(".question");
-question.textContent = secretnumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   let guessvalue = Number(document.querySelector(".add").value);
-  if (guessvalue === "") {
+  hit.textContent = "";
+
+  if (!guessvalue) {
     start.textContent = "📢 Input a number, its blank!!";
   } else if (guessvalue === secretnumber) {
     start.textContent = "🎉 Correct number!";
+    question.textContent = secretnumber;
+    hit.textContent = "👈 You won, play again?"
+
+    score > highscore ? (highscore = score) : "none";
+    hig.textContent = highscore;
+
+    document.querySelector(".container").style.backgroundColor = "#60b347";
+    document.querySelector("question").style.width = "100px";
   } else if (score > 0) {
-    if (guessvalue > secretnumber) {
-      start.textContent = "📈 Too high";
-      score--;
-      number.textContent = score;
-    } else if (guessvalue < secretnumber) {
-      start.textContent = "📉 Too low";
-      score--;
-      number.textContent = score;
-    } else if (score === 0) {
-      start.textContent = "👎 You lose";
-    }
-  } else {
-    start.textContent = "📢 Input a number, its blank!!";
+    guessvalue > secretnumber
+      ? (start.textContent = "📈 Too high")
+      : (start.textContent = "📉 Too low");
+    score--;
+
+    number.textContent = score;
+  } 
+  
+  if(score === 0){
+  start.textContent = "👎 You lose "
+  document.querySelector(".container").style.backgroundColor = "red";
+
   }
+
+  score === 0 && start.textContent === "👎 You lose "
+    ? (hit.textContent = "👈Hit the again button")
+    : "none";
 });
 
 document.querySelector(".again").addEventListener("click", function () {
@@ -37,4 +54,9 @@ document.querySelector(".again").addEventListener("click", function () {
   number.textContent = score;
   secretnumber = Math.trunc(Math.random() * 20 + 1);
   question.textContent = secretnumber;
+  hit.textContent = "Now input a number😇";
+  question.textContent = '❓';
+    document.querySelector(".container").style.backgroundColor = " #000000de";
+    
+
 });
